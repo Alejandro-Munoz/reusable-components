df55feb41c7414a6f5203d449c218677450f0c95
Alejandro Munoz <alejandro.z.munoz@gmail.com>
2025-12-04 14:06:59 -0800

    mb2

--- src/components/Link.js
+++ src/components/Link.js
@@ -4,7 +4,7 @@ import useNavigation from '../hooks/useNavigation';
 export default function Link({ to, children }) {
   const { navigate } = useNavigation();
 
-  const classes = classNames('text-blue-300')
+  const classes = classNames('text-blue-300', 'mb-2')
 
   const handleClick = (e) => {
     if (e.metaKey || e.ctrlKey) {
