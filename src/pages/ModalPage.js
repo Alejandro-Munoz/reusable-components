5a4e393c58fff0fac7e41f10f5df8c44bb98559a
Alejandro Munoz <alejandro.z.munoz@gmail.com>
2026-01-29 22:49:56 -0800

    modal start

---
+++ src/pages/ModalPage.js
@@ -0,0 +1,19 @@
+import Modal from "../components/Modal";
+import Button from "../components/Button";
+import { useState } from "react";
+
+function ModalPage() {
+  const [showModal, setShowModal] = useState(false)
+
+  const handleClick = () => {
+    setShowModal(true)
+  }
+
+  return (
+  <div>
+    <Button onClick={handleClick} primary>Open modal</Button>
+    {showModal && <Modal />}
+  </div>);
+}
+
+export default ModalPage;Modal
