d7365f3b5d9d8778b1d3fe30036ad8f975edc755
Alejandro Munoz <alejandro.z.munoz@gmail.com>
2026-01-29 22:11:21 -0800

    navigation wrapup

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,14 @@
 import classNames from 'classnames'
 import useNavigation from '../hooks/useNavigation';
 
-export default function Link({ to, children }) {
-  const { navigate } = useNavigation();
+export default function Link({ to, children, className, activeClassName }) {
+  const { navigate, currentPath } = useNavigation();
 
-  const classes = classNames('text-blue-400', 'mb-3')
+  const classes = classNames(
+    'text-blue-500',
+    className,
+    currentPath === to && activeClassName
+  )
 
   const handleClick = (e) => {
     if (e.metaKey || e.ctrlKey) {
